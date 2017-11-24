db1c92e5d0b1e06c78e82fca02749598a6fb117b
muhammad-taha1 <muhammad.taha@mail.mcgill.ca>
2017-11-23 23:22:04 -0500

    adding parallax effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { DeliveryComponent } from './delivery/delivery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    MenuComponent,
+    DeliveryComponent
   ],
   imports: [
     BrowserModule
